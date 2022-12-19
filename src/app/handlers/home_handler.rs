426eb79e30dfbc6b21bf210631baccccc7d79f12
kundan <kundan.karna1994@gmail.com>
2022-12-19 22:51:03 +0545

    adding custom message to response json

--- src/app/handlers/home_handler.rs
+++ src/app/handlers/home_handler.rs
@@ -1,4 +1,4 @@
-use axum::Json;
+use axum::{http::StatusCode, Json};
 use serde::{Deserialize, Serialize};
 
 pub async fn index() -> String {
@@ -13,7 +13,15 @@ pub async fn mirror_body_string(body: String) -> String {
 pub struct Payload {
     message: String,
 }
-pub async fn mirror_body_json(Json(payload): Json<Payload>) -> Json<Payload> {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MirrorPayloadResponse {
+    message: String,
+    custom_message: String,
+}
+pub async fn mirror_body_json(Json(payload): Json<Payload>) -> Json<MirrorPayloadResponse> {
     println!("{:?}", payload);
-    Json(payload)
+    Json(MirrorPayloadResponse {
+        message: payload.message,
+        custom_message: "This is custom message from server".to_owned(),
+    })
 }
