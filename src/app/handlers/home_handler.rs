177f7f62c964103219b5f0646a4e193214f4555b
kundan <kundan.karna1994@gmail.com>
2022-12-19 22:35:03 +0545

    mirror body text

--- src/app/handlers/home_handler.rs
+++ src/app/handlers/home_handler.rs
@@ -1,3 +1,7 @@
 pub async fn index() -> String {
     "Hello From the Server".to_owned()
 }
+
+pub async fn mirror_body_string(body: String) -> String {
+    body
+}
--- src/app/routes/home.rs
+++ src/app/routes/home.rs
@@ -2,5 +2,7 @@ use crate::app::handlers::home_handler;
 use axum::{routing::get, Router};
 
 pub fn routes() -> Router {
-    Router::new().route("/", get(home_handler::index))
+    Router::new()
+        .route("/", get(home_handler::index))
+        .route("/mirror_body_string", get(home_handler::mirror_body_string))
 }
