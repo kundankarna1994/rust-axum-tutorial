dd04e58108d9cc40b0f14aaf424e511fcaf45cd8
Kundan <kundan.karna1994@gmail.com>
2022-12-20 12:37:52 +0545

    cors layer

--- src/app/routes/mod.rs
+++ src/app/routes/mod.rs
@@ -1,5 +1,7 @@
 use axum::Router;
+
 mod home;
+
 pub fn routes() -> Router {
     Router::new().nest("/", home::routes())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use app::routes;
+use axum::http::Method;
+use tower_http::cors::{Any, CorsLayer};
 mod app;
 
 pub async fn run() {
-    let app = routes::routes();
+    let cors = CorsLayer::new()
+        .allow_methods([Method::POST])
+        .allow_origin(Any);
+    let app = routes::routes().layer(cors);
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
